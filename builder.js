45c8b2075cdeef6322383d7268112b0ac0eb5c08
Yaroslav Surilov <y.surilov@infomir.com>
2019-06-17 21:46:20 +0300

    add dummy favicon for absent ones

--- builder.js
+++ builder.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const
-    fs           = require('fs'),
-    htmlMinifier = require('html-minifier'),
-    sourceData   = require('./src/data'),
-    readFile     = path => fs.readFileSync(path, 'utf-8'),
+    fs             = require('fs'),
+    htmlMinifier   = require('html-minifier'),
+    sourceData     = require('./src/data'),
+    readFile       = path => fs.readFileSync(path, 'utf-8'),
+    encodeToBase64 = path => new Buffer(fs.readFileSync(path)).toString('base64'),
+
     // TODO: research on options -- https://www.npmjs.com/package/html-minifier
     htmlMinifierOptions = {
         removeComments:                 true,
@@ -44,15 +46,23 @@ if ( fs.existsSync('./node_modules/hack/dist/hack.css') ) {
 // TODO: rework -- add flexibility, templates map as a parameter
 function buildHTMLString ( contentItems ) {
     const
-        header = readFile('./src/html.templates/header.tpl'),
-        footer = readFile('./src/html.templates/footer.tpl');
+        header         = readFile('./src/html.templates/header.tpl'),
+        footer         = readFile('./src/html.templates/footer.tpl'),
+        unknownFavicon = `data:image/png;base64,${encodeToBase64('./src/images/favicon.unknown.png')}`;
 
     let body = '';
 
     contentItems.forEach((item, index) => {
         const faviconPath = item.uri.replace(/^(https?:\/\/[^/]+).*$/, '$1/favicon.ico');
 
-        body += `<tr><td>${index}</td><td><img src="${faviconPath}"></td><td>${item.description}</td><td><a href="${item.uri}" target="_blank">Get it!</a></td></tr>`;
+        let favicon = new Image();
+
+        favicon.onerror = () => favicon.src = unknownFavicon;
+
+        //favicon.classList.add('favicon');
+        favicon.src = faviconPath;
+
+        body += `<tr><td>${index}</td><td>${favicon}</td><td>${item.description}</td><td><a href="${item.uri}" target="_blank">Get it!</a></td></tr>`;
     });
 
     return header.replace('{css}', cssString) + body + footer;
